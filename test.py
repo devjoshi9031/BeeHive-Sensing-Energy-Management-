06f712deb130b9cb1bfbfd693da044d39da102a9
Dev Joshi <devjoshi9031@gmail.com>
2022-10-14 13:21:52 -0700

    Working for normal mode and longer period mode

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import os, sys, time
 
-
-E_des = 4.00
-
 period = [10,20,30,40,50,60]
 
-def calculate_error(E_recv):
-    error = E_des - E_recv
+def calculate_error(desired, recv):
+    error = desired - recv
     return error
 
 def get_value_from_sensor():
@@ -65,31 +62,45 @@ def calculate_stride_start_stop_normal_period(expected) -> tuple:
 
 
 def main():
+    desired_voltage = 4.23
     print("PRINTING LARGER PERIOD VERSION:")
-    start,stop,stride,error = calculate_stride_start_stop_larger_period(4.00)
+    start,stop,stride,error = calculate_stride_start_stop_larger_period(desired_voltage)
     print("Stride is: {:.2f}\t{:.2f}\t{:.2f}".format(start, stop, stride))
     for i in error:
         print("{:.2f}".format(4.00-i))
     
     
-    print("PRINTING SMALLER PERIOD VERSION:")
-    start,stop,stride,error = calculate_stride_start_stop_smaller_period(4.00)
-    print("Stride is: {:.2f}\t{:.2f}\t{:.2f}".format(start, stop, stride))
-    for i in error:
-        print("{:.2f}".format(4.00-i))
+    # print("PRINTING SMALLER PERIOD VERSION:")
+    # start,stop,stride,error = calculate_stride_start_stop_smaller_period(4.00)
+    # print("Stride is: {:.2f}\t{:.2f}\t{:.2f}".format(start, stop, stride))
+    # for i in error:
+    #     print("{:.2f}".format(i))
 
     print("PRINTING NORMAL PERIOD VERSION:")
-    start,stop,stride,error = calculate_stride_start_stop_normal_period(4.00)
+    start,stop,stride,error = calculate_stride_start_stop_normal_period(desired_voltage)
     print("Stride is: {:.2f}\t{:.2f}\t{:.2f}".format(start, stop, stride))
     for i in error:
         print("{:.2f}".format(4.00-i))
 
-
+    a = [0]*6
     for i in get_value_from_sensor():
         if(i <= 3.3):
             break
-        error = calculate_error(i)
-        print("Error: {}".format(error))
+        error_val = calculate_error(desired_voltage, i)
+        for i in range(0,len(error)):
+            if(error[i]>error_val):
+                actual_error = error[i]
+                # print("Error: {}\t Index: {}")
+                p = period[i-1]
+                a[i-1]+=1
+                break
+        print("Error: {}\t Period: {}".format(actual_error, p))
+        time.sleep(p/1000)
+        
+    total=0
+    for i in a:
+        total += i
+    print(a,total) 
     
     # change_period(error)
 
